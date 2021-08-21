acc5cf2879181b5939460bff79bd7f204d64d825
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-21 23:59:00 +0000

    2nd commit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const printPassTimes = function(passTimes) {
     console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
 };
+
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
